39ad5a95e8a499e20c92036d6044defdf5a57cd3
Kellandor <ppham@csumb.edu>
2016-12-01 10:49:52 -0800

    Finished level (i think)

--- source.cpp
+++ source.cpp
@@ -1260,7 +1260,7 @@ void InitGame() {
 		currentLevel->blocks.push_back(new Platform(i, -1.0, 0.05, g_ground, 0));
 	}
 
-	for (float i = 26.0; i < 28.0; i += 0.1)
+	for (float i = 26.0; i <= 28.0; i += 0.1)
 	{
 		currentLevel->blocks.push_back(new Platform(i, -0.9, 0.05, g_stoneBlockTex, 0));
 	}
@@ -1280,6 +1280,20 @@ void InitGame() {
 	currentLevel->spikes.push_back(new Spike(23.09, -0.8, 0.05, g_spike3, 0));
 
 	currentLevel->blocks.push_back(new Platform(23.2, -0.9, 0.05, g_stoneBlockTex, 3));
+
+	currentLevel->blocks.push_back(new Platform(24.0, -0.8, 0.05, g_stoneBlockTex, 0));
+	currentLevel->spikes.push_back(new Spike(24.0, -0.71, 0.05, g_spike, 0));
+	currentLevel->spikes.push_back(new Spike(23.91, -0.8, 0.05, g_spike2, 0));
+	currentLevel->spikes.push_back(new Spike(24.09, -0.8, 0.05, g_spike3, 0));
+
+	currentLevel->blocks.push_back(new Platform(24.2, -0.9, 0.05, g_stoneBlockTex, 3));
+
+	currentLevel->blocks.push_back(new Platform(25.0, -0.8, 0.05, g_stoneBlockTex, 0));
+	currentLevel->spikes.push_back(new Spike(25.0, -0.71, 0.05, g_spike, 0));
+	currentLevel->spikes.push_back(new Spike(24.91, -0.8, 0.05, g_spike2, 0));
+	currentLevel->spikes.push_back(new Spike(25.09, -0.8, 0.05, g_spike3, 0));
+
+	currentLevel->blocks.push_back(new Platform(25.2, -0.9, 0.05, g_stoneBlockTex, 3));
 
 	for (float i = 21.4; i < 26.0; i += 0.1)
 	{
@@ -1362,6 +1376,36 @@ void InitGame() {
 	for (float i = -0.3; i <= 0.9; i += 0.1)
 	{
 		currentLevel->blocks.push_back(new Platform(26.0, i, 0.05, g_ground, 0));
+	}
+
+	for (float i = 26.0; i <= 27.8; i += 0.1)
+	{
+		currentLevel->blocks.push_back(new Platform(i, 0.9, 0.05, g_ground, 0));
+	}
+
+	for (float i = 26.1; i <= 27.8; i += 0.1)
+	{
+		currentLevel->blocks.push_back(new Platform(i, 0.3, 0.05, g_ground, 0));
+	}
+
+	for (float i = 26.1; i <= 27.8; i += 0.1)
+	{
+		currentLevel->blocks.push_back(new Platform(i, -0.3, 0.05, g_ground, 0));
+	}
+
+	for (float i = -0.3; i <= 1.5; i += 0.1)
+	{
+		currentLevel->blocks.push_back(new Platform(28.0, i, 0.05, g_ground, 0));
+	}
+
+	for (float i = 26.3; i <= 28.0; i += 0.1)
+	{
+		currentLevel->blocks.push_back(new Platform(i, 0.6, 0.05, g_ground, 0));
+	}
+
+	for (float i = 26.3; i <= 28.0; i += 0.1)
+	{
+		currentLevel->blocks.push_back(new Platform(i, 0.0, 0.05, g_ground, 0));
 	}
 
 	currentLevel->doors.push_back(new Door(27.0, -0.8, 0.09, g_checkPoint));
